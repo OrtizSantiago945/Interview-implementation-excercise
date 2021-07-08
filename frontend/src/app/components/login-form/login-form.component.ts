679df3ea3ff1d7d2d36090ad2e5d5ccc2204fb26
santiortizutn <santyno09@gmail.com>
2021-07-08 14:02:20 -0300

    login changes

--- frontend/src/app/components/login-form/login-form.component.ts
+++ frontend/src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  email : string;
+  password : string;
+
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    if(this.email == 'user@gmail.com' && this.password == '123456'){
+      Swal.fire({
+        toast: true,
+        icon: 'success',
+        text: 'Welcome!',
+        showConfirmButton: false,
+        position: 'bottom',
+        timer: 1000
+      }).then(() => this.router.navigate(['/home']));
+    }else{
+      Swal.fire({
+        toast: true,
+        icon: 'error',
+        text: 'Invalid credentials',
+        showConfirmButton: false,
+        position: 'bottom',
+        timer: 1500
+      })
+    }
   }
 
 }
